98a3ba40fff1aba5b46636546c5baf1bfefccf8d
Kavya <135150115+CaptainCode2024@users.noreply.github.com>
2025-09-02 19:24:58 +0530

    Add: LeetCode #1148 [Article Views I]

---
+++ Problems/LeetCode/Database/leetcode_1148_article-views-i.sql
@@ -0,0 +1,21 @@
+/**
+ * Platform: LeetCode
+ * Problem: 1148. Article Views I
+ * Link: https://leetcode.com/problems/article-views-i/
+ * Difficulty: Easy
+ * Solved: 01-09-2025
+ *
+ * Approach:
+ * - Need authors who viewed their own articles → `author_id = viewer_id`
+ * - Used DISTINCT to remove duplicates
+ * - Sorted results in ascending order
+ *
+ * Complexity Analysis:
+ * - Time: O(N) [Scan each row once]
+ * - Space: O(1) [No extra memory beyond query execution]
+ */
+
+SELECT DISTINCT author_id as id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id;  -- sort
